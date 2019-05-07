57dec7789f84baedbbe1943508551b8203253479
Mysticlegend <45702579+Mysticlegend@users.noreply.github.com>
2019-05-07 12:04:26 +0700

    Add files via upload
    datescript

---
+++ datett..js
@@ -0,0 +1,13 @@
+
+<!--
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+var myDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var date = new Date();
+var day = date.getDate();
+var month = date.getMonth();
+var thisDay = date.getDay(),
+    thisDay = myDays[thisDay];
+var yy = date.getYear();
+var year = (yy < 1000) ? yy + 1900 : yy;
+document.write(thisDay + ', ' + day + ' ' + months[month] + ' ' + year);
+//-->
